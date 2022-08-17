ef0d76ce649d9d37e5a01c1fa7b5d53979f180ac
Legebrind <moyi88@gmail.com>
2022-08-17 17:32:26 +0000

    Bottom style added

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -6,7 +6,7 @@ import List from "./list.jsx";
 //create your first component
 const Home = () => {
 	return (
-		<div className="container-fluid bg-secondary.bg-gradient w-25">
+		<div className="container-fluid bg-secondary.bg-gradient w-25 mt-5 shadows">
 			<List/>
 		</div>
 	);
--- src/js/component/list.jsx
+++ src/js/component/list.jsx
@@ -1,35 +1,66 @@
 import React, { useState } from "react";
 
-const List = () =>{
-    const [inputValue, setInputValue] = useState("");
-    const [items, setItems]  = useState([]);
+const List = () => {
+  const [inputValue, setInputValue] = useState("");
+  const [items, setItems] = useState([]);
 
-    function addItem(){
-        if(!inputValue){
-            alert("Item can't be empty");
-            return
-        }
-        setItems((prevList) =>[...prevList,inputValue]);
-        console.log(items);
-        setInputValue("");
+  function addItem() {
+    if (!inputValue) {
+      alert("Item can't be empty");
+      return;
     }
-    return(
-        <div className="list">
-            <h1>To-Do List</h1>
-            <input type="text" placeholder="Add an item" value={inputValue} onChange={e=> setInputValue(e.target.value)}/>
-            <button onClick={()=>addItem()}>Add</button>
-            <ul>
-                {items.length==0?
-                    <li>List is empty, add a task</li> : 
-                    items.map((item,i)=>{
-                        return(
-                            <li key={i}>{item}</li>
-                        )
-                    })
-                }
-            </ul>
-        </div>
-    )
-}
+    setItems((prevList) => [...prevList, inputValue]);
+    setInputValue("");
+  }
+  function handleRemove(e) {
+    const lista = document.querySelector("#list");
+    lista.removeChild(e.target);
+  }
+  return (
+    <div className="d-inline-flex flex-column w-100 container justify-content-center align-items-center shadows">
+      <div className="row">
+        <h1 className="col-12">To-Do List</h1>
+      </div>
+
+      <div className="row mt-3">
+        <input
+          className="col-8"
+          size="60"
+          type="text"
+          placeholder="Add an item"
+          value={inputValue}
+          onChange={(e) => setInputValue(e.target.value)}
+        />
+        <button className="col-4" onClick={() => addItem()}>
+          Add
+        </button>
+      </div>
+
+      <ul id="list" className="list-group col-12 mt-3">
+        {items.length == 0 ? (
+          <li className="list-group-item text-center">
+            List is empty, add a task
+          </li>
+        ) : (
+          items.map((item, i) => {
+            return (
+              <li
+                className="list-group-item text-center"
+                id={i}
+                key={i}
+                onClick={handleRemove}
+              >
+               <span className="hide"><i className="fa fa-trash"/></span>{item}
+              </li>
+            );
+          })
+        )}
+        <li className="list-group-item text-center text-black-50">{`${items.length} item left`}</li>
+        <div className="fondo1"></div>
+        <div className="fondo2"></div>
+      </ul>
+    </div>
+  );
+};
 
 export default List;
